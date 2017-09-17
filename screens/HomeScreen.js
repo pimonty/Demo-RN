8c1121fa286c169a75dd9e5ae917f862f99b7cf5
Unknown <pimonty@gmail.com>
2017-09-17 23:29:23 +0100

    Defined place for the list of products.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -23,38 +23,11 @@ export default class HomeScreen extends React.Component {
         <ScrollView
           style={styles.container}
           contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View
-              style={[
-                styles.codeHighlightContainer,
-                styles.homeScreenFilename,
-              ]}>
-              <MonoText style={styles.codeHighlightText}>
-                screens/HomeScreen.js
-              </MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
+          <Text>List of products.</Text>
 
           <View style={styles.helpContainer}>
+
             <TouchableOpacity
               onPress={this._handleHelpPress}
               style={styles.helpLink}>
