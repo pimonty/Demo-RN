8aa2be4f51c068aedbf6debdc543b19d1d9fa7f5
Unknown <pimonty@gmail.com>
2017-09-18 19:34:29 +0100

    Connection by URL, get JSON and display basic data.

--- screens/HomeListView/HomeLView.js
+++ screens/HomeListView/HomeLView.js
@@ -12,36 +12,38 @@ import {
 } from 'react-native';
 
 
-
-//Define de style of list View
 const styles = StyleSheet.create({
   Listview: {
     flex: 1,
     padding: 12,
     height: 290,
+
     },
+
 });
+
+
 
 
 class HomeLView extends Component {
 
-  //Define the states
+
   constructor(props) {
     super(props);
 
+    //Define state
     this.state={
       isLoading: true
     };
 
-    console.log('Constructor-> State isloading:'+ this.state.isLoading);
+
   }
 
 
   componentDidMount() {
+    const PathJson='http://server/products/product.json'
 
-    const PathJson = 'http://server/products/product.json'
-
-    return fetch('http://192.168.1.69:3013/v1/productos.json')
+    return fetch(PathJson)
       .then((response) => response.json())
       .then((responseJson) => {
         try{
@@ -50,44 +52,37 @@ class HomeLView extends Component {
           isLoading: false,
           dataSource: ds.cloneWithRows(responseJson.productos),
         }, function() {
-          // nothing to do with new state.
+          // nothing to do with new state
+          console.log('All data read from:'+PathJson);
         });
-      } catch(e){
-        alert('Exception while processing. \nPlease,try again.');
-      }
-      })
 
+        }catch(e){
+          alert('Exception while processing. \nPlease,try again.');
+        }
+
+      })
       .catch((error) => {
         alert('Connection error:\n\n'+PathJson+'\n\nPlease, check yout network connection and path.')
 
-        //Stop the icon wait
-        this.setState({
-          isLoading: false
-        }, function() {
-          // nothing to do with new state.
-          console.log('exception isloading:'+this.state.isLoading)
-        });
+      //Stop the icon wait
+      this.setState({
+        isLoading: false
+      }, function() {
+        // nothing to do with new state.
+        console.log('Catch error:'+this.state.isLoading);
+      });
 
-        console.error(error);
       });
   }
 
-
-
 render() {
-
-console.log('Render-> State isloading:'+ this.state.isLoading);
-
    if (this.state.isLoading) {
      return (
-
        <View style={{flex: 1, paddingTop: 20}}>
          <ActivityIndicator />
        </View>
-
      );
    }
-
 
    return (
      <View style={{flex: 1, paddingTop: 20}}>
@@ -95,12 +90,11 @@ console.log('Render-> State isloading:'+ this.state.isLoading);
          style={styles.Listview}
          dataSource={this.state.dataSource}
 
-        //Show initial dates.
-        renderRow={(rowData) => <Text style={styles.text}>{rowData.identificador}, {rowData.price}</Text>}
-
+         //Inicialize each row
+         renderRow={(rowData) => <Text style={styles.text}>{rowData.identificador}, {rowData.price}</Text>}
        />
-     </View>
 
+     </View>
    );
  }
 
