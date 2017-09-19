ec1387f6c1a7dc3e0c428372c42eca585abcbac2
Unknown <pimonty@gmail.com>
2017-09-19 16:39:00 +0100

    Update references and screen of tab "link".

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { TouchableHighlight, Text, ScrollView, StyleSheet } from 'react-native';
+import { Dimensions,Image,TouchableHighlight, Text, ScrollView, StyleSheet } from 'react-native';
 //import { ExpoLinksView } from '@expo/samples';
  //import Link from '../components/links/Link'
  import { WebBrowser } from 'expo';
+
+
+
+
+let winSize = Dimensions.get('window');
+const UriPicture='http://tmnopal.com.mx/tmnopal-archivos/2015/11/comercializacion-de-producto.png';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -12,13 +18,14 @@ export default class LinksScreen extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-
+        <Image source={require('./Logogithub.png')} style={styles.photo} />
+        {/* <Image source={{ uri: UriPicture}} style={styles.photo} /> */}
         <TouchableHighlight onPress={this._linkReactn} style={styles.reactn}>
-          <Text>Tutorial React native</Text>
+          <Text style={styles.txt}>Tutorial React native</Text>
         </TouchableHighlight>
 
         <TouchableHighlight onPress={this._linkMern} style={styles.mern}>
-          <Text>Tutorial MERN</Text>
+          <Text style={styles.txt}>Tutorial MERN</Text>
         </TouchableHighlight>
 
       </ScrollView>
@@ -47,21 +54,43 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff'
+    paddingTop: 90,
+    backgroundColor: '#eee',
+    flexDirection: 'column',
   },
   reactn: {
-    flex: 1,
-    paddingTop: 60,
+    alignSelf: 'center',
+    paddingTop: 50,
+    borderRadius: 30,
+    borderWidth: 1.5,
     backgroundColor: '#3746',
+    borderColor: '#d6d7da',
     height:120,
-    width:200,
+    width:winSize.width,
+    alignItems: 'center',
   },
   mern: {
-    flex: 1,
-    paddingTop: 60,
-    backgroundColor: '#6321',
+    alignSelf: 'center',
+    borderRadius: 30,
+    borderWidth: 1.5,
+    borderColor: '#d6d7da',
+    paddingTop: 50,
+    backgroundColor: '#79b2e7',
     height:120,
-    width:200,
+    width:winSize.width,
+    alignItems: 'center',
+  },
+  txt:{
+    fontSize: 19,
+    fontWeight: 'bold',
+    alignItems:'center',
+  },
+  photo: {
+    height: 0.15*winSize.height,
+    width: 0.8
+    *winSize.width,
+    borderRadius: 20,
+    alignSelf: 'center',
   }
+
 });
